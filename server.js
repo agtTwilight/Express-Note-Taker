8d6cc80fd2414b62ad573957af14c5a91d14cbba
agtTwilight <lukasmacmillen@gmail.com>
2023-01-30 12:30:08 -0800

    post request retrieves user inputs

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 3000;
 const data = require('./db/db.json')
 
+// fix the request issue i had
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // set file paths to .../public/... by default
 app.use(express.static("public"));
 
@@ -19,6 +23,13 @@ app.get('/notes', (req, res) => {
 // display api
 app.get('/api/notes', (req, res) => {
         res.json(data);
+})
+
+// let index.js post to db
+app.post('/api/notes', (req, res) => {
+        // all this does is log something into json file on post
+        res.json(`${req.body}`)
+        console.log(req.body);
 })
 
 app.listen(PORT, function () {
