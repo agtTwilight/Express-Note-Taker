6dafb83c87c0787638452471e86572d190f5c628
agtTwilight <lukasmacmillen@gmail.com>
2023-01-30 16:42:25 -0800

    added note id and delete note by id function

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const generateUniqueId = require('generate-unique-id');
 const express = require('express');
+const fs = require('fs');
+let noteData = require('./db/db.json')
 const app = express();
 const PORT = 3000;
-const data = require('./db/db.json')
 
 // fix the request issue i had
 app.use(express.json());
@@ -22,16 +24,59 @@ app.get('/notes', (req, res) => {
 
 // display api
 app.get('/api/notes', (req, res) => {
-        res.json(data);
+        res.json(noteData);
 })
 
 // let index.js post to db
 app.post('/api/notes', (req, res) => {
-        // all this does is log something into json file on post
-        res.json(`${req.body}`)
-        console.log(req.body);
+        // TODO add content that tells the user what is happening on post requests
+        // get the user input
+        const {title, text} = req.body;
+
+        // create new note w/ user note data & randomly generated ID
+        const newNote = {
+                title,
+                text,
+                id: generateUniqueId(),
+        }
+
+        // add new note obj to existing db
+        noteData.push(newNote);
+
+        // update the db file
+        fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => {
+                if (err) {
+                        res.status(500).send("oh no!");
+                        throw err;
+                      } else {
+                        res.send("data added!");
+                      }
+        })
+
 })
 
+// delete note
+app.delete(`/api/notes/:id`, (req, res) => {
+        noteData = noteData.filter((note) => {
+                if (note.id == req.params.id) {
+                        return false;
+                } else {
+                        return true
+                }
+        })
+
+        // update the db file
+        fs.writeFile('./db/db.json', JSON.stringify(noteData), (err) => {
+                if (err) {
+                        res.status(500).send("oh no!");
+                        throw err;
+                      } else {
+                        res.send("data added!");
+                      }
+        })
+})
+
+// listen for when the local port is opened.
 app.listen(PORT, function () {
         console.log(`Listening at http://localhost:${PORT}`)
 });
