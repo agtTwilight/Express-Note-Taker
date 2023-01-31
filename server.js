1ba4b006c51a1094c4cfabf26a1c1379624ae49b
agtTwilight <lukasmacmillen@gmail.com>
2023-01-30 16:51:04 -0800

    heroku port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 let noteData = require('./db/db.json')
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // fix the request issue i had
 app.use(express.json());
